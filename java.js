a7fc7e481b0949b9f0aab7b09c3218152983f58b
dalonatt <mac@dalonatt-2.local>
2022-09-15 16:42:47 +0700

    first commit

---
+++ java.js
@@ -0,0 +1,20 @@
+/* darkmode */
+var theme = document.getElementById("darkmode");
+
+var storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme)
+    
+theme.onclick = function() {
+    var currentTheme = document.documentElement.getAttribute("data-theme");
+    var targetTheme = "light";
+
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+
+};
